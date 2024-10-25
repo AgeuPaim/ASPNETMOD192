0d32eab72477939184055a764b0df7786474fd76
Ageu Paim <ageupaim@gmail.com>
2024-10-25 09:38:19 +0100

    Add project files.

---
+++ ASPNETMOD192/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ASPNETMOD192.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETMOD192.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; } = default!;// tenta que exista uma
+                                                              // tabela e inser os dados, tenta criar uma instancias com valores pardão
+    }
+}
